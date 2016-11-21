e92c32a7c782a8cfa1fef1421c7d151e1dbc12ee
Abhi Khanal <abhi@imoonlight.io>
2016-11-21 14:47:53 -0500

    decoupled the class.

--- DeckSystem/ViewController.swift
+++ DeckSystem/ViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, DeckViewDataSource {
+
+    //    var data = [UIColor.red, UIColor.blue, UIColor.yellow, UIColor.green]
+
+//    var data = [UIColor.red, UIColor.blue, UIColor.black]
+    //    var data = [UIColor.red, UIColor.blue]
+        var data = [UIColor.red]
+
 
     @IBOutlet weak var deck: UIView!
 
@@ -19,9 +26,12 @@ class ViewController: UIViewController {
         if let deck = UINib.init(nibName: "DeckView", bundle: Bundle.main)
             .instantiate(withOwner: nil, options: nil).first as? DeckView {
 
+            deck.dataSource = self
+
             print("VIEW WIDTH: \(view.frame.width)")
 
             deck.bind(to: CGRect(x: 0, y: 0, width: self.deck.frame.width, height: 239))
+
             self.deck.addSubview(deck)
         }
     }
@@ -29,6 +39,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSlidesIn(_ deckView: DeckView) -> Int {
+        return data.count
+    }
+
+    func slideFor(_ deckView: DeckView, at position: Int) -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: deck.frame.width, height: deck.frame.height - 50))
+        view.backgroundColor = data[position]
+        return view
     }
 
 
